c6e484f8198da87125adff230d66a84ee98fd0a9
ndwarshuis <ndwar@yavin4.ch>
2021-02-15 21:28:58 -0500

    REF add a useful note justifying the existence of this script

--- run_once_clone-repos.sh
+++ run_once_clone-repos.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+## This script pulls all my git repos that I use in my config. It will only pull
+## them if they don't exist. Rather than use the import approach suggested in
+## the chezmoi howto, this doesn't require me to keep 'syncing' changes when I
+## work in these repos directly. The tradeoff is that it will only pull the
+## latest master, which is totally fine with me.
+
 clone_maybe () {
     if [ ! -d "$2" ]; then
         echo cloning git repo: "$1"
