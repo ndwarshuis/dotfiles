ccc17c70d43ee8f9c6e18391d0abe60daf0a2b2c
ndwarshuis <ndwar@yavin4.ch>
2021-03-13 16:07:10 -0500

    ENH call install package script as root

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -4,48 +4,63 @@
 ## Configuration is assumed to be handled elsewhere (for now) eg in etckeeper
 ## or with ansible
 
-PKGBUILD_dir="$HOME/.local/share/packages"
+sudo bootstrap_pkgs
 
-call_makepkg() {
-    cd "$PKGBUILD_dir/$1" || exit
-    makepkg -s -r -i -f --noconfirm
-}
+# PKGBUILD_dir="$HOME/.local/share/packages"
 
-## install packages (those that are either in official repos or AUR)
+# call_makepkg() {
+#     cd "$PKGBUILD_dir/$1" || exit
+#     sudo -u nobody makepkg -s -r -i -f --noconfirm
+# }
 
-## TODO add template switches to control which of these get installed based
-## on my config
-dunst_pkgs=(dunst)
-emacs_pkgs=(emacs mu)
-flameshot_pkgs=(flameshot)
-gtk_pkgs=(zuki-themes)
-nvidia_pkgs=(optimus-manager)
-r_pkgs=(r docker-rootless-extras-bin gcc-fortran texlive-bin tk)
-redshift_pkgs=(redshift)
-rofi_pkgs=(rofi-git bitwarden-cli libnotify rofi-greenclip
-           networkmanager-dmenu-git veracrypt sshfs jmtpfs)
-seafile_pkgs=(seafile)
-urxvt_pkgs=(urxvt-tabbedex rxvt-unicode urxvt-perls)
-xmonad_pkgs=(stack-static autorandr feh xorg-server xorg-xset libpulse playerctl
-             wireless_tools acpid ttf-symbola-free ttf-symbola-free ttf-dejavu
-             awesome-terminal-fonts numlockx picom i3lock-color xorg-xrandr
-             xss-lock)
-zsh_pkgs=(zsh zsh-completions zsh-syntax-highlighting)
+# ## install packages (those that are either in official repos or AUR)
 
-## AUR pkgs needed for spotify
-spotify_pkgs=(gconf)
+# ## TODO add template switches to control which of these get installed based
+# ## on my config
+# dunst_pkgs=(dunst)
+# emacs_pkgs=(emacs mu)
+# flameshot_pkgs=(flameshot)
+# gtk_pkgs=(zuki-themes)
+# nvidia_pkgs=(optimus-manager)
+# r_pkgs=(r docker-rootless-extras-bin gcc-fortran texlive-bin tk)
+# redshift_pkgs=(redshift)
+# rofi_pkgs=(rofi-git bitwarden-cli libnotify rofi-greenclip
+#            networkmanager-dmenu-git veracrypt sshfs jmtpfs)
+# seafile_pkgs=(seafile)
+# urxvt_pkgs=(urxvt-tabbedex rxvt-unicode urxvt-perls)
+# xmonad_pkgs=(stack-static autorandr feh xorg-server xorg-xset libpulse playerctl
+#              wireless_tools acpid ttf-symbola-free ttf-symbola-free ttf-dejavu
+#              awesome-terminal-fonts numlockx picom i3lock-color xorg-xrandr
+#              xss-lock)
+# zsh_pkgs=(zsh zsh-completions zsh-syntax-highlighting)
 
-sudo -v
+# ## AUR pkgs needed for spotify
+# spotify_pkgs=(gconf)
 
-yay --noconfirm --removemake -Syy --sudoloop "${dunst_pkgs[@]}" \
-    "${emacs_pkgs[@]}" "${flameshot_pkgs[@]}" "${gtk_pkgs[@]}" \
-    "${nvidia_pkgs[@]}" "${nvidia_pkgs[@]}" "${r_pkgs[@]}" \
-    "${redshift_pkgs[@]}" "${rofi_pkgs[@]}" "${seafile_pkgs[@]}" \
-    "${urxvt_pkgs[@]}" "${xmonad_pkgs[@]}" "${zsh_pkgs[@]}" "${spotify_pkgs[@]}"
+# if sudo -u nobody -n sudo pacman -Syy; then
+#     echo "Using user nobody for installation. Sit back and relax."
+# else
+#     echo "Using current user for installation. Prepare to enter sudo pwd alot."
+# fi
 
-## install custom packages (eg those for which I have my own PKGBUILDs)
+# sudo -u nobody yay --noconfirm --removemake -S "${dunst_pkgs[@]}" \
+#     "${emacs_pkgs[@]}" \
+#     "${flameshot_pkgs[@]}" \
+#     "${gtk_pkgs[@]}" \
+#     "${nvidia_pkgs[@]}" \
+#     "${nvidia_pkgs[@]}" \
+#     "${r_pkgs[@]}" \
+#     "${redshift_pkgs[@]}" \
+#     "${rofi_pkgs[@]}" \
+#     "${seafile_pkgs[@]}" \
+#     "${urxvt_pkgs[@]}" \
+#     "${xmonad_pkgs[@]}" \
+#     "${zsh_pkgs[@]}" \
+#     "${spotify_pkgs[@]}"
 
-call_makepkg "clevo-xsm-wmi-dkms"
-call_makepkg "conky-lua"
-call_makepkg "spotify"
-call_makepkg "xkb-hypermode"
+# ## install custom packages (eg those for which I have my own PKGBUILDs)
+
+# call_makepkg "clevo-xsm-wmi-dkms"
+# call_makepkg "conky-lua"
+# call_makepkg "spotify"
+# call_makepkg "xkb-hypermode"
