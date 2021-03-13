3bce3139d83e4f9af4fbb8ae191bb55076f298ed
ndwarshuis <ndwar@yavin4.ch>
2021-03-13 17:31:08 -0500

    FIX actually tell bootstrap script where the custom packages are

--- run_once_after_install-packages.sh
+++ run_once_after_install-packages.sh
@@ -4,7 +4,7 @@
 ## Configuration is assumed to be handled elsewhere (for now) eg in etckeeper
 ## or with ansible
 
-sudo "$HOME/.bin/bootstrap_pkgs"
+sudo "$HOME/.bin/bootstrap_pkgs $HOME/.local/share/packages"
 
 # PKGBUILD_dir="$HOME/.local/share/packages"
 
