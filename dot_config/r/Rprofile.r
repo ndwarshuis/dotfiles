79bd2e5b09f353e4b58a4e59bc186bc3e233bec0
ndwarshuis <ndwar@yavin4.ch>
2021-04-24 00:19:28 -0400

    ENH update rofi devs and rprofile

--- dot_config/r/Rprofile.r
+++ dot_config/r/Rprofile.r
@@ -1,5 +1,11 @@
-.First <- function() {
+## .First <- function() {
 ##  message("R is the best\n","working directory is:", getwd())
+## }
+
+## TODO make this find .Rprofiles in parent directories
+if ( ".Rprofile" %in% list.files(all.files = TRUE) ) {
+    message("Sourcing local .Rprofile")
+    source(".Rprofile")
 }
 
 ## Set CRAN mirror:
