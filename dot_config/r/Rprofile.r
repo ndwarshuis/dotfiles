4d0f966d122201bc12f7be39fe8a5dae9ebf42ed
ndwarshuis <ndwar@yavin4.ch>
2022-07-20 20:04:11 -0400

    ENH make nice config file to configure which apps/vars I need

--- dot_config/r/Rprofile.r
+++ dot_config/r/Rprofile.r
@@ -1,7 +1,3 @@
-## .First <- function() {
-##  message("R is the best\n","working directory is:", getwd())
-## }
-
 ## TODO make this find .Rprofiles in parent directories
 if ( ".Rprofile" %in% list.files(all.files = TRUE) ) {
     message("Sourcing local .Rprofile")
